bff4d51d2f9836c9f2908477b40bc31a5c0c346f
Amair299 <amairb299@gmail.com>
2025-01-22 10:40:34 +0530

    Added sample test in branchX

---
+++ src/test/java/com/selectorshub/Sample1.java
@@ -0,0 +1,12 @@
+package com.selectorshub;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	
+	@Test
+	public void sampleTest() {
+		System.out.println("Sample test passed");
+	}
+
+}
